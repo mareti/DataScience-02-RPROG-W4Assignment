d4a1ad0e9ac3734d4ac9d811635ba0047d47ff9b
Muralidhar Areti <Muralidhar.Areti@outlook.com>
2015-05-30 16:15:22 -0400

    Submission 1, 2
    Successfully got the submit() for 1 and 2. In its current form 3 didn’t
    work because the test case NN is not in the list…

--- best.R
+++ best.R
@@ -3,34 +3,33 @@
 
 best <- function(state=NA, outcome=NA) {
     ## Read outcome data 
-    data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
-    err = 0
+    data <- read.csv("outcome-of-care-measures.csv"
+                     , na.strings="Not Available"
+                     #, colClasses="character"
+                     )
     
     ## Check that state and outcome are valid
     if (is.na(state) || nchar(state)>2) {
-        # stop("invalid state")
-        # print("invalid state")
-        err = err + 1
-    }
-    
-    if (is.na(outcome)
-        || !outcome==c("heart attack","heart failure","pneumonia")) {
-        # stop("invalid outcome")
-        # print("invalid outcome")
-        err = err + 2
-    }
-    
-    if (err == 1) 
         stop("invalid state")
-    else if (err == 2)
+        # print("invalid state")
+        # err = err + 1
+    }
+    
+    if (outcome=="heart failure")
+        i = 17
+    else if (outcome=="heart attack") 
+        i = 11
+    else if (outcome=="pneumonia")
+        i = 23
+    else { 
+        # err = err + 2
         stop("invalid outcome")
-    else if (err == 3)
-        stop("invalid state and outcome")
-    
-    
+    }
     
     ## Return hospital name in that state with lowest 30-day death rate
-    print("The function works... ish...")
-    print(state)
-    print(outcome)
+    y = subset(data.frame(data[c(2,7,i)]), data$State==state)
+    z = y[ which(y[3]==min(y[3], na.rm=TRUE)), ]
+    z = z[order(z[1])]
+    # z$Hospital.Name
+    as.character(print(z[1,], row.names=FALSE, max.levels=0))
 }
