e65fba623ac5b2843d4d226fbe41a9d69bdf3485
Muralidhar Areti <Muralidhar.Areti@outlook.com>
2015-05-30 22:03:32 -0400

    rankhospital - attempt 1
    Got 5,6,7 submitted successfully. Need to work on the “worst” part…

--- best.R
+++ best.R
@@ -9,11 +9,8 @@ best <- function(state=NA, outcome=NA) {
                      )
     
     ## Check that state and outcome are valid
-    # if (is.na(state) || nchar(state)>2) {
     if (state %in% data$State == FALSE) {
         stop("invalid state")
-        # print("invalid state")
-        # err = err + 1
     }
     
     if (outcome=="heart failure")
@@ -23,14 +20,13 @@ best <- function(state=NA, outcome=NA) {
     else if (outcome=="pneumonia")
         i = 23
     else { 
-        # err = err + 2
         stop("invalid outcome")
     }
     
     ## Return hospital name in that state with lowest 30-day death rate
     y = subset(data.frame(data[c(2,7,i)]), data$State==state)
     z = y[ which(y[3]==min(y[3], na.rm=TRUE)), ]
-    z = z[order(z[1])]
+    z = z[ order(z[1]), ]
     # z$Hospital.Name
     as.character(print(z[1,], row.names=FALSE, max.levels=0))
 }
--- rankhospital.R
+++ rankhospital.R
@@ -1,10 +1,27 @@
-rankhospital(state=NA, outcome=NA) {
+rankhospital <- function(state=NA, outcome=NA, num="best") {
     ## Read outcome data
-    
+    data <- read.csv("outcome-of-care-measures.csv"
+                     , na.strings="Not Available"
+                     #, colClasses="character"
+                     )
     
     ## Check that state and outcome are valid
+    if (state %in% data$State == FALSE) {
+        stop("invalid state")
+    }
     
+    if (outcome=="heart failure")       i = 17
+    else if (outcome=="heart attack")   i = 11
+    else if (outcome=="pneumonia")      i = 23
+    else stop("invalid outcome")
     
     ## Return hospital name in that state with the given rank 30-day death rate
+    y = subset(data.frame(data[c(2,7,i)]), data$State==state)
+    colnames(y) = c("Name", "State", "Rate")
+    y = y[ order( y[3], y[1], na.last=TRUE, decreasing=FALSE ), ]
     
+    if (num=="best") num=1
+    if (num=="worst") num=2
+    
+    as.character(print(y[num,1], row.names=FALSE, max.levels=0))
 }
