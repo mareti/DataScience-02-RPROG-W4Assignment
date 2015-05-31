532f2d19d8d95a99f0f6ddae71ca25860df0ef17
Muralidhar Areti <Muralidhar.Areti@outlook.com>
2015-05-31 01:32:21 -0400

    rankall v1
    Got question 8 and 10, having trouble with 9…

--- rankall.R
+++ rankall.R
@@ -1,13 +1,29 @@
-rankall <- function(outcome, num="best") {
+rankall <- function(outcome=NA, num="best") {
     ## Read outcome data
-    
+    data <- read.csv("outcome-of-care-measures.csv"
+                     , na.strings="Not Available"
+                     #, colClasses="character"
+                     )
     
     ## Check that state(?) and outcome are valid
-    
+    if (outcome=="heart failure")       i = 17
+    else if (outcome=="heart attack")   i = 11
+    else if (outcome=="pneumonia")      i = 23
+    else stop("invalid outcome")
     
     ## For each state, find the hospital of the given rank
+    y = subset( data.frame( data[ c(2,7,i) ] ) )
+    colnames(y) = c("hospital", "state", "rate")
+    y = y[ order( y[3], y[1], na.last=TRUE, decreasing=FALSE ), ]
+    y$rank = ave(y$rate, y$state
+                 , FUN = function(x) rank(x, ties.method="first"))
     
+    if (num=="best") num=1
+    if (num=="worst") num=max(y$rank, na.rm=TRUE)
+    
+    y = subset(y, y$Rank == num)
     
     ## Return a data frame with the hospital names and the state name
-    
+    # print(y[num,1], row.names=FALSE, max.levels=0)
+    y[, 1:2]
 }
