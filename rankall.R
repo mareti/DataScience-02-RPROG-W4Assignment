f11fa730617ba64c8476cbf9529299c1528785a6
Muralidhar Areti <Muralidhar.Areti@outlook.com>
2015-05-31 11:48:16 -0400

    rankall v1.1
    Still don’t have #9 working…

--- rankall.R
+++ rankall.R
@@ -14,14 +14,23 @@ rankall <- function(outcome=NA, num="best") {
     ## For each state, find the hospital of the given rank
     y = subset( data.frame( data[ c(2,7,i) ] ) )
     colnames(y) = c("hospital", "state", "rate")
-    y = y[ order( y[3], y[1], na.last=TRUE, decreasing=FALSE ), ]
+
+    if (num=="best") { 
+        num = 1
+        y = y[ order( y[3], y[1], na.last=TRUE, decreasing=FALSE ), ]
+    }
+    else if (num=="worst") {
+        num = 1
+        y = y[ order( -y[3], y[1], na.last=FALSE, decreasing=FALSE ), ]
+    }
+    else {
+        y = y[ order( y[3], y[1], na.last=TRUE, decreasing=FALSE ), ]
+    }
+    
     y$rank = ave(y$rate, y$state
                  , FUN = function(x) rank(x, ties.method="first"))
     
-    if (num=="best") num=1
-    if (num=="worst") num=max(y$rank, na.rm=TRUE)
-    
-    y = subset(y, y$Rank == num)
+    y = subset(y, y$rank == num)
     
     ## Return a data frame with the hospital names and the state name
     # print(y[num,1], row.names=FALSE, max.levels=0)
